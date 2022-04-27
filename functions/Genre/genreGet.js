8e2ee101e58a08ae1d667d57baca8ba986d798db
Gabriel <gabrielignaciosanchez99@gmail.com>
2022-04-27 16:10:22 -0300

    last commit

--- functions/Genre/genreGet.js
+++ functions/Genre/genreGet.js
@@ -1,6 +1,6 @@
 const { Genre } = require("../../db.js");
 
-const notFound = { response: { message: "Movie not found" }, status: 404 };
+const notFound = { response: { message: "Genre not found" }, status: 404 };
 
 const responseFunction = (r) => {
   if (r.length > 0) return { response: r, status: 200 };
